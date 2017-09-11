055244f296a22247ea0e794a3f317289ecda1e06
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-11 16:59:30 +0200

    [YIL-311] Initial commit.

---
+++ src/main/scripts/win32/build.sh
@@ -0,0 +1,19 @@
+cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+make install
+r1=$?
+
+make clean
+
+rm -R CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
+rm install_manifest.txt
+rm -R utils
+rm config.h
+rm openal.pc
+rm OpenALConfig.cmake
+rm version.h
+
+return $r1
